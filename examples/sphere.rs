a2ffecafb0349971c33b8abe1386ae26bff46a01
Téo <teo.orthlieb@gmail.com>
2025-05-18 10:07:07 +0200

    update rust & bevy

--- examples/sphere.rs
+++ examples/sphere.rs
@@ -30,7 +30,7 @@ fn main() {
                 }),
                 ..default()
             }),
-            WireframePlugin,
+            WireframePlugin::default(),
         ))
         .add_systems(Startup, setup)
         .run();
@@ -71,6 +71,7 @@ fn setup(
     commands.insert_resource(AmbientLight {
         color: Color::WHITE,
         brightness: light_consts::lux::OVERCAST_DAY,
+        ..Default::default()
     });
 }
 
