7b418fb81c16590782ed7804a9623b4d8b378708
Nathanael Neveux <nathanaelneveux@me.com>
2025-05-18 07:53:22 -0400

    added second bench to show main path performance hasn't changed

--- benches/mesh_32.rs
+++ benches/mesh_32.rs
@@ -1,16 +1,21 @@
+
 use std::collections::BTreeSet;
 
 use binary_greedy_meshing as bgm;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
+const CHUNK_SIZE: usize = 32;
+const CS_H: usize = CHUNK_SIZE/2;
 const SIZE: usize = 16;
 const SIZE2: usize = SIZE.pow(2);
 
-fn voxel_buffer() -> Box<[u16; bgm::CS_P3]> {
-    let mut voxels = Box::new([0; bgm::CS_P3]);
-    for x in 0..bgm::CS {
-        for y in 0..bgm::CS {
-            for z in 0..bgm::CS {
-                voxels[bgm::pad_linearize(x, y, z)] = sphere(x, y, z);
+use bgm::MeshDataGeneric as MD;
+
+fn voxel_buffer() -> Box<[u16; MD::<CHUNK_SIZE>::CS_P3]> {
+    let mut voxels = Box::new([0; MD::<CHUNK_SIZE>::CS_P3]);
+    for x in 0..CHUNK_SIZE {
+        for y in 0..CHUNK_SIZE {
+            for z in 0..CHUNK_SIZE {
+                voxels[bgm::pad_linearize_sized::<CHUNK_SIZE>(x, y, z)] = sphere(x, y, z);
             }
         }
     }
@@ -18,41 +23,41 @@ fn voxel_buffer() -> Box<[u16; bgm::CS_P3]> {
 }
 
 fn sphere(x: usize, y: usize, z: usize) -> u16 {
-    if (x as i32-31).pow(2) + (y as i32-31).pow(2) + (z as i32-31).pow(2) < SIZE2 as i32 {
+    if (x - CS_H).pow(2) + (y - CS_H).pow(2) + (z - CS_H).pow(2) < SIZE2 {
         1
     } else {
         0
     }
 }
 
-fn bench_opaque(c: &mut Criterion) {
+fn bench_opaque_32(c: &mut Criterion) {
     let voxels = voxel_buffer();
-    let mut mesh_data = bgm::MeshData::new();
+    let mut mesh_data = bgm::MeshDataSized::<CHUNK_SIZE>::new();
     c.bench_function("bench_opaque", |b| b.iter(|| {
         mesh_data.clear();
-        bgm::mesh(
+        bgm::mesh_sized::<CHUNK_SIZE>(
             black_box(voxels.as_slice()), black_box(&mut mesh_data), black_box(BTreeSet::default())
         );
     }));
 }
 
-fn bench_transparent(c: &mut Criterion) {
+fn bench_transparent_32(c: &mut Criterion) {
     let voxels = voxel_buffer();
     let mut transparents = BTreeSet::default();
     transparents.insert(2);
     transparents.insert(3);
-    let mut mesh_data = bgm::MeshData::new();
+    let mut mesh_data = bgm::MeshDataSized::<CHUNK_SIZE>::new();
     c.bench_function("bench_transparent", |b| b.iter(|| {
         mesh_data.clear();
-        bgm::mesh(
+        bgm::mesh_sized::<CHUNK_SIZE>(
             black_box(voxels.as_slice()), black_box(&mut mesh_data), black_box(BTreeSet::default())
         );
     }));
 }
 
 criterion_group!(
-    mesh, 
-    bench_opaque, 
-    bench_transparent
+    mesh_32, 
+    bench_opaque_32, 
+    bench_transparent_32
 );
-criterion_main!(mesh);
+criterion_main!(mesh_32);
